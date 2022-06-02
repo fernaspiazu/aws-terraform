a93cf780dc7f1ada291037fb7a3bc93b07bcdffc
Fernando A <fe.aspiazu@gmail.com>
2022-06-02 21:20:05 +0200

    Try to create main-vpc

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = var.region
+
+  assume_role {
+    # The role ARN to AssumeRole
+    role_arn = var.arn_role
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 variable "region" {}
 
+variable "arn_role" {}
+
 variable "vpc_name" {
   default = "main-vpc"
 }
