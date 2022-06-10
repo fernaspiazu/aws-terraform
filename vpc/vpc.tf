c84b71b6389eb771301ec9b661bf65529a49df90
Fernando A <fe.aspiazu@gmail.com>
2022-06-11 00:02:43 +0200

    Reconfigured VPC

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/8"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -7,6 +7,11 @@ resource "aws_vpc" "main_vpc" {
     Name = var.vpc_name,
     Description = "Main VPC"
   }
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "cidr_10_161" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.161.0.0/16"
 }
 
 resource "aws_internet_gateway" "main_igw" {
