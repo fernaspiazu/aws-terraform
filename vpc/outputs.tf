1794d503374095ad04c5e8def0b402be90da7c62
Fernando A <fe.aspiazu@gmail.com>
2022-07-23 19:37:12 +0200

    Create private subnets

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
+output "public_subnets_id" {
+  value = ["${aws_subnet.public_subnet.*.id}"]
+}
+
+output "private_subnets_id" {
+  value = ["${aws_subnet.private_subnet.*.id}"]
 }
